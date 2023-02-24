46498ab798dbb52b466c7d9063cdf580004da76f
Rahul vinod <rahulvinod135@gmail.com>
2023-02-24 12:55:20 +0530

    ending game, adding restart logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,32 @@
+function resetGameStatus() {
+    activePlayer = 0;
+    currentRound = 1;
+    gameIsOver = false;
+    gameOverElement.firstElementChild.innerHTML = 
+   'You Won!, <span id="winner-name">PLAYER NAME</span>!';
+    gameOverElement.style.display = 'none';
+
+    let gameBoardIndex = 0;
+    for (let i = 0; i <3; i++){
+        for (let j =0; j < 3; j++) {
+            gameData[i][j] = 0;
+            const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+            gameBoardItemElement.textContent = '';
+            gameBoardItemElement.classList.remove('disabled');
+            gameBoardIndex++;
+        }
+    }
+}
+
+
 function startNewGame() {
     if (players[0].name === '' || players[1].name === '') {
         alert('Please set custom player name for both players!');
         return;
     }
+
+    resetGameStatus();
+
     activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = 'block';
 }
@@ -17,7 +41,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-    if (event.target.tagName !== 'LI') {
+    if (event.target.tagName !== 'LI' || gameIsOver) {
         return;
     }
 
@@ -36,10 +60,14 @@ function selectGameField(event) {
 
 
     gameData[selectedrow][selectedColumn] = activePlayer + 1;
-    
+
     const winnerId = checkGameOver();
-    console.log(winnerId);
-    
+    if (winnerId !== 0 ) {
+       endGame(winnerId);  
+    }
+   
+
+
     currentRound++;
     switchPlayer();
 }
@@ -70,8 +98,8 @@ function checkGameOver() {
         return gameData[0][0];
     }
 
-     //Diagonal: Bottom left to top right
-     if (gameData[2][0] > 0 &&
+    //Diagonal: Bottom left to top right
+    if (gameData[2][0] > 0 &&
         gameData[2][0] === gameData[1][1] &&
         gameData[1][1] === gameData[0][2]) {
         return gameData[2][0];
@@ -81,4 +109,17 @@ function checkGameOver() {
     }
 
     return 0;
+}
+ 
+function endGame(winnerId) {
+    gameIsOver = true;
+    gameOverElement.style.display = 'block';
+
+    if (winnerId > 0) {
+        const winnerName = players[winnerId - 1].name;
+        gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
+    } else {
+        gameOverElement.firstElementChild.textContent = 'it\'s a Draw!'
+    }
+
 }
